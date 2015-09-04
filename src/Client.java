25d3396ac3bab0d52103d369a5cf8d430f94b3de
chancepianta <chancepianta@gmail.com>
2015-09-03 17:31:47 -0700

    implementation of tcp client

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+
+	public static String doTCPRequest(String command, String host, int port) {
+		String returnValue = "";
+		Socket server = null;
+		try {
+			server = new Socket(host, port);
+			BufferedReader bReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			PrintStream printStream = new PrintStream(server.getOutputStream());
+			printStream.println(command);
+			printStream.flush();
+			returnValue = bReader.readLine();
+		} catch (Exception e) {
+			returnValue = e.getMessage();
+		} finally {
+			if ( server != null && !server.isClosed() ) {
+				try { server.close(); } catch (Exception e) {}
+			}
+		}
+		return returnValue;
+	}
+	
+	public static String doUDPRequest(String command, String host, int port) {
+		return "NOT IMPLEMENTED";
+	}
+	
   public static void main (String[] args) {
     String hostAddress;
     int tcpPort;
@@ -23,18 +52,22 @@ public class Client {
       String cmd = sc.nextLine();
       String[] tokens = cmd.split(" ");
 
-      if (tokens[0].equals("reserve")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("bookSeat")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("delete")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
+      if (tokens[0].equals("reserve") && tokens.length >= 3) {
+        System.out.println( 
+        		( "T".equals( tokens[2] ) ? doTCPRequest( cmd, hostAddress, tcpPort ) : 
+        			( "U".equals( tokens[2] ) ? doUDPRequest( cmd, hostAddress, udpPort ) : "ERROR: Bad command" ) ) );
+      } else if (tokens[0].equals("bookSeat") && tokens.length >= 4) {
+    	  System.out.println( 
+          		( "T".equals( tokens[3] ) ? doTCPRequest( cmd, hostAddress, tcpPort ) : 
+          			( "U".equals( tokens[3] ) ? doUDPRequest( cmd, hostAddress, udpPort ) : "ERROR: Bad command" ) ) );
+      } else if (tokens[0].equals("search") && tokens.length >= 3) {
+    	  System.out.println( 
+          		( "T".equals( tokens[2] ) ? doTCPRequest( cmd, hostAddress, tcpPort ) : 
+          			( "U".equals( tokens[2] ) ? doUDPRequest( cmd, hostAddress, udpPort ) : "ERROR: Bad command" ) ) );
+      } else if (tokens[0].equals("delete") && tokens.length >= 3) {
+    	  System.out.println( 
+          		( "T".equals( tokens[2] ) ? doTCPRequest( cmd, hostAddress, tcpPort ) : 
+          			( "U".equals( tokens[2] ) ? doUDPRequest( cmd, hostAddress, udpPort ) : "ERROR: Bad command" ) ) );
       } else {
         System.out.println("ERROR: No such command");
       }
